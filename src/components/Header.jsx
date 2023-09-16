1c2fcb00b78711b3cb9969790a51f7584118132a
Richard Guilliams <dropletswebdevelopment@gmail.com>
2023-09-16 17:08:47 -0600

    Fixed links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ const Header = function(){
         <>
             <div className='header'>
                 <img className='header-logo' src='./img/logo.png' width='40px' height='40px' alt='Rick Guilliams Tree Service Logo'></img>
-                <a className='header-cta' href='tel:5134737370'>Call: 513-473-7370</a>
                 <NavBar/>    
             </div>        
         </>
